71f0ae5934b375bd2e5b14458af9f75cfe601ebb
Aditya Bimawan <aditditto@gmail.com>
2020-08-24 22:53:19 +0700

    Add delete button and update read status button

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 //inputs
-const submitBtn = document.getElementById('newBook');
+const submitForm = document.getElementById('new-book-form');
 const titleBox = document.getElementById('title');
 const authorBox = document.getElementById('author');
 const pagesBox = document.getElementById('pages');
 const yesRadio = document.getElementById('yes');
+const formShow = document.getElementById('form-show');
+const formContainer = document.getElementById('form-container');
 
 //table columns
 const titleCol = document.getElementById('title-col');
 const authorCol = document.getElementById('author-col');
 const pagesCol = document.getElementById('pages-col');
 const readCol = document.getElementById('read-col');
+const delCol = document.getElementById('del-col')
 
 let myLibrary = [];
 
@@ -22,6 +25,10 @@ function Book(title, author, pages, read) {
 
 Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'already read' : 'not read yet'}`
+}
+
+Book.prototype.changeReadStatus = function() {
+    this.read = this.read ? false : true
 }
 
 function zip(a, b) {
@@ -39,38 +46,77 @@ function createBook() {
     myLibrary.push(new Book(title, author, pages, read))
 }
 
-function clearForm() {
-    [titleBox, authorBox, pagesBox].forEach(box => {
-        box.value = ''
-    })
-}
-
 function render() {
-    myLibrary.forEach(book => {
-        let pairs = zip(Object.values(book).slice(0, 3), [titleCol, authorCol, pagesCol])
+    myLibrary.forEach((book, index) => {
+        //rendering title author and pages
+        let pairs = zip([book.title, book.author, book.pages], [titleCol, authorCol, pagesCol])
         pairs.forEach(pair => {
             let val = pair[0]; let container = pair[1];
             let newP = document.createElement('p')
             newP.textContent = String(val)
             container.appendChild(newP)
         })
-        let newP = document.createElement('p')
-        newP.textContent = Object.values(book)[4] ? 'Yes' : 'No'
+        //rendering read or not
+        let newP = document.createElement('button')
+        console.log(`${book.title} ${book.read}`)
+        newP.textContent = book.read ? 'Yes' : 'No'
+        newP.setAttribute('data-idx', String(index))
+        newP.addEventListener('click', updateRead)
         readCol.appendChild(newP)
+        //rendering delete button
+        let newDel = document.createElement('button')
+        newDel.textContent = 'X'
+        newDel.setAttribute('data-idx', String(index))
+        newDel.addEventListener('click', removeRow)
+        delCol.appendChild(newDel)
     })
 }
 
-function addBook() {
-    createBook()
-    clearForm()
+function clearCols() {
+    [authorCol, titleCol, pagesCol, readCol, delCol].forEach(column => {
+        column.innerHTML = ''
+    })
+}
+
+function removeRow(e) {
+    index = parseInt(e.target.getAttribute('data-idx'))
+    myLibrary.splice(index, 1)
+    clearCols()
     render()
 }
 
-submitBtn.addEventListener('click', addBook)
+function updateRead(e) {
+    index = parseInt(e.target.getAttribute('data-idx'))
+    book = myLibrary[index]
+    book.changeReadStatus()
+    e.target.textContent = book.read ? 'Yes' : 'No'
+}
+
+function addBook(e) {
+    e.preventDefault();
+    clearCols()
+    createBook()
+    submitForm.reset()
+    render(myLibrary.slice(-1))
+}
+
+formContainer.style.display = 'none';
+function showHideForm() {
+    console.log(formContainer.style.display)
+    if (formContainer.style.display !== 'none') {
+        formContainer.style.display = 'none';
+    }
+    else {
+        formContainer.style.display = 'block';
+    }
+}
+
+submitForm.addEventListener('submit', addBook)
+
+formShow.addEventListener('click', showHideForm)
 
 book1 = new Book('poop', 'me', 69, 'Yes')
 myLibrary.push(book1)
 
-console.log(book1.info())
-console.log(myLibrary)
+render()
 console.log(Object.values(book1))
